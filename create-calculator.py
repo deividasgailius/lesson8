7219d9b228a44181735b3f93f1c9451f6ddf0063
Deividas Gailius <deividasgailius86@gmail.com>
2023-10-10 19:59:42 +0300

    add calculator statement

---
+++ create-calculator.py
@@ -0,0 +1,13 @@
+a = int(input("enter digit: "))
+b = int(input("enter digit: "))
+c = input("enter your simbol: ")
+if c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
+else:
+    print("not")
